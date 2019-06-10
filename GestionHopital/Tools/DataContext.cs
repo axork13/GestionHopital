2ed52bb7e028514d22e8aba270cf4603f83deaa4
Jimmy Marmin <jimmy.marmin@gmail.com>
2019-06-10 15:18:21 +0200

    Mise en place du DataContext

---
+++ GestionHopital/Tools/DataContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionHopital.Tools
+{
+    public class DataContext : DbContext
+    {
+        private static object _lock = new object();
+        private static DataContext _instance = null;
+        private static readonly string connectionString = @"";
+
+        public static DataContext Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                        _instance = new DataContext();
+                    return _instance;
+                }
+            }
+        }
+
+        private DataContext() : base (connectionString)
+        {
+
+        }
+    }
+}
