56a6dac0a0b795b11bc64a12e0d0997878b007eb
Jimmy Marmin <jimmy.marmin@gmail.com>
2019-06-10 15:30:57 +0200

    Création des DTO

---
+++ GestionHopital/Models/DTO/Traitements/Examen_radiologique.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionHopital.Models.DTO.Traitements
+{
+    public class Examen_radiologique
+    {
+    }
+}
