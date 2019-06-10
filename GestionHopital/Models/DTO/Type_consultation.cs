059f8f733e9c014db25444ebd99245b5ef63b2a8
Jimmy Marmin <jimmy.marmin@gmail.com>
2019-06-10 17:52:59 +0200

    Modification du DTO Type_consultation

--- GestionHopital/Models/DTO/Type_consultation.cs
+++ GestionHopital/Models/DTO/Type_consultation.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace GestionHopital.Models.DTO
 {
-    public class Type_consultation
+    public class Type_consultation : BaseDTO
     {
+        private int id_type_consultation;
+        private string type_consultation;
+        private decimal prix_consultation;
+
+        private int consultation_id;
+
+        public Type_consultation()
+        {
+        }
+
+        public Type_consultation(int id_type_consultation, string type_consultation, decimal prix_consultation, int consultation_id)
+        {
+            this.id_type_consultation = id_type_consultation;
+            this.type_consultation = type_consultation;
+            this.prix_consultation = prix_consultation;
+            this.consultation_id = consultation_id;
+        }
+
+        public override bool CheckData()
+        {
+            return (type_consultation == "" || type_consultation == null || prix_consultation < 0) ? false : true; 
+        }
+
+        public override string ToString()
+        {
+            return "Type consultation (id:" + id_type_consultation + ", type:" + type_consultation + ", prix:" + prix_consultation + "€";
+        }
     }
 }
