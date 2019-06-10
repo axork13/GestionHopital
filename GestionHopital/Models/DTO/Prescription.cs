a2f7cc37c15662c3d23279b3812e819dc75dd863
Jimmy Marmin <jimmy.marmin@gmail.com>
2019-06-10 17:49:12 +0200

    Modification des DTO Rendez_vous et Prescription

--- GestionHopital/Models/DTO/Prescription.cs
+++ GestionHopital/Models/DTO/Prescription.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace GestionHopital.Models.DTO
 {
-    public class Prescription
+    public class Prescription : BaseDTO
     {
+        private int id_prescription;
+        private DateTime date_prescription;
+        private string nom_patient;
+        private string prenom_patient;
+        private string note;
+
+        private int consultation_id;
+        private int medecin_id;
+
+        public Prescription()
+        {
+        }
+
+        public Prescription(int id_prescription, DateTime date_prescription, string nom_patient, string prenom_patient, string note, int consultation_id, int medecin_id)
+        {
+            this.id_prescription = id_prescription;
+            this.date_prescription = date_prescription;
+            this.nom_patient = nom_patient;
+            this.prenom_patient = prenom_patient;
+            this.note = note;
+            this.consultation_id = consultation_id;
+            this.medecin_id = medecin_id;
+        }
+
+        public override bool CheckData()
+        {
+            return (date_prescription == null || nom_patient == "" || nom_patient == null || prenom_patient == "" || prenom_patient == null) ? false : true;
+        }
+
+        public override string ToString()
+        {
+            return "Prescription (id :" + id_prescription + " date de prescription: " + date_prescription +
+                "\n nom du patient: " + nom_patient + "prenom du patient:" + prenom_patient + " Note: " + note + ")";
+        }
     }
 }
--- GestionHopital/Models/DTO/Rendez-vous.cs
+++ GestionHopital/Models/DTO/Rendez-vous.cs
@@ -6,7 +6,59 @@ using System.Threading.Tasks;
 
 namespace GestionHopital.Models.DTO
 {
-    public class Rendez_vous
+    public enum ServiceEnum
     {
+        Cardiologie,
+        Neurologie,
+        Hematologie,
+        Pneumologie,
+        Nephrologie,
+        Maternite,
+        Osteopathie,
+        Oncologie
+    }
+
+    public class Rendez_vous : BaseDTO
+    {
+        private int code_rdv;
+        private string medecin;
+        private DateTime date;
+        private ServiceEnum service;
+
+        private int patient_id;
+        private int consultation_id;
+
+        public Rendez_vous()
+        {
+        }
+
+        public Rendez_vous(int code_rdv, string medecin, DateTime date, ServiceEnum service, int patient_id, int consultation_id)
+        {
+            this.code_rdv = code_rdv;
+            this.medecin = medecin;
+            this.date = date;
+            this.service = service;
+            this.patient_id = patient_id;
+            this.consultation_id = consultation_id;
+        }
+
+        public int Code_rdv { get => code_rdv; set => code_rdv = value; }
+        public string Medecin { get => medecin; set => medecin = value; }
+        public DateTime Date { get => date; set => date = value; }
+        public ServiceEnum Service { get => service; set => service = value; }
+        public int Patient_id { get => patient_id; set => patient_id = value; }
+        public int Consultation_id { get => consultation_id; set => consultation_id = value; }
+
+        public override bool CheckData()
+        {
+            return (Enum.IsDefined(typeof(ServiceEnum), service) || medecin == "" || medecin == null || patient_id < 0 || date == null) ? false : true;
+        }
+
+        public override string ToString()
+        {
+            return "Rendez_Vous (Code Rendez-Vous :" + code_rdv + "Nom du Medecin:" + medecin + " Date du Rdv:" + date + " Service :" + service + ")";
+        }
+
+       
     }
 }
