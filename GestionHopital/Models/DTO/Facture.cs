b895b2d7edd6a51ddf3c712e6c6817cf15b76f58
Jimmy Marmin <jimmy.marmin@gmail.com>
2019-06-10 17:03:24 +0200

    Modification du DTO Facture

--- GestionHopital/Models/DTO/Facture.cs
+++ GestionHopital/Models/DTO/Facture.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace GestionHopital.Models.DTO
 {
-    public class Facture
+    public class Facture : BaseDTO
     {
+        private int id_facture;
+        private DateTime date_facture;
+        private decimal total;
+
+        private int admission_id;
+        private int traitement_id;
+
+        public Facture()
+        {
+        }
+
+        public Facture(int id_facture, DateTime date_facture, decimal total, int admission_id, int traitement_id)
+        {
+            this.id_facture = id_facture;
+            this.date_facture = date_facture;
+            this.total = total;
+            this.admission_id = admission_id;
+            this.traitement_id = traitement_id;
+        }
+
+        public int Id_facture { get => id_facture; set => id_facture = value; }
+        public DateTime Date_facture { get => date_facture; set => date_facture = value; }
+        public decimal Total { get => total; set => total = value; }
+        public int Admission_id { get => admission_id; set => admission_id = value; }
+        public int Traitement_id { get => traitement_id; set => traitement_id = value; }
+
+        public override bool CheckData()
+        {
+            return date_facture == null ? false : true;
+        }
+
+        public override string ToString()
+        {
+            return "Facture (id :" + id_facture + ", date : " + date_facture + ", total : " + total + ")";
+        }
     }
 }
