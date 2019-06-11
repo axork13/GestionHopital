7969ac1b0c2dc06eb6795525fdcd4e73a67040b6
Jimmy Marmin <jimmy.marmin@gmail.com>
2019-06-11 16:17:31 +0200

    Création de l'interface IDAO

---
+++ GestionHopital/Models/DAO/IDAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionHopital.Models.DAO
+{
+    public interface IDAO<T, ID>
+    {
+        bool Create(T t);
+        T Retrieve(ID id);
+        bool Update(T t, ID id);
+        List<T> ListAll();
+    }
+}
