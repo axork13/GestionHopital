a026e5de1a53e0d1b5939626fccd45d7333e9865
Jimmy Marmin <jimmy.marmin@gmail.com>
2019-06-11 19:23:21 +0200

    Ajout de la DAO PatientDAO

---
+++ GestionHopital/Models/DAO/PatientDAO.cs
@@ -0,0 +1,46 @@
+﻿using GestionHopital.Models.DTO;
+using GestionHopital.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionHopital.Models.DAO
+{
+    public class PatientDAO : IDAO<Patient, int>
+    {
+        private DataContext data;
+
+        public PatientDAO()
+        {
+            data = DataContext.Instance;
+        }
+
+        public bool Create(Patient t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Patient Retrieve(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Patient t, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Patient> ListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
